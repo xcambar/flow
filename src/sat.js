b9cbdd9afaf5c6a39b987e27d26dc4654a095e13
Xavier Cambar <xcambar@gmail.com>
2012-09-24 01:01:26 +0200

    SAT can now analyze itself

--- src/sat.js
+++ src/sat.js
@@ -8,45 +8,50 @@
 (function (window, doc) {
   "use strict";
   var _onLoadCallbacks = [];
-  window.addEventListener('load', function blocksAnyCall () {
+
+  function blocksAnyCall () {
     if (typeof window.onload === 'function') {
       _onLoadCallbacks.push(window.onload);
       window.onload = null;
     }
-    var satScripts = doc.querySelectorAll('sat-script');
-    var remainingScripts = satScripts.length;
-    Array.prototype.slice.call(satScripts).forEach(function (scriptNode) {
-      var xhr = new XMLHttpRequest();
-      var name = scriptNode.getAttribute('src');
-      xhr.open('GET', name, true);
-      xhr.onreadystatechange = function () {
-        if (this.readyState === 4) {
-          remainingScripts--;
-          _runFlow(this.responseText, (scriptNode.getAttribute('data-flow') || '').split(','), name);
-        }
-        if (!remainingScripts) {
-          _runLoadCallbacks();
-        }
-      };
-      xhr.send();
-    });
-
+    var satFlows = Array.prototype.slice.call(doc.querySelectorAll('sat-flow'));
+    _runNextFlow();
+    function _runNextFlow () {
+      var next = satFlows.shift();
+      if (next) {
+        _runFlow(next);
+      } else {
+        _runLoadCallbacks();
+      }
+    }
+    function _runFlow (flow) {
+      var flowSteps = Array.prototype.slice.call(flow.children);
+      var runFlows = 0;
+      flowSteps.forEach(function (scriptNode, index, all) {
+        var totalLength = all.length;
+        var xhr = new XMLHttpRequest();
+        var name = scriptNode.getAttribute('src');
+        xhr.open('GET', name, true);
+        xhr.onreadystatechange = function () {
+          if (this.readyState === 4) {
+            _applyFlow(this.responseText, (scriptNode.getAttribute('data-steps') || '').split(','), name);
+          }
+          if (++runFlows === flowSteps.length) {
+            _runNextFlow();
+          }
+        };
+        xhr.send();
+      });
+    }
     function _runLoadCallbacks () {
       _onLoadCallbacks.forEach(function (fn) {
         setTimeout(fn, 1); // So we keep asynchronicity
       });
     }
-  });
+  }
+  window.addEventListener('load', blocksAnyCall);
 
-  window.addEventListener = function onLoadWrapper (eventType, callback, useCapture) {
-    if (eventType === 'load') {
-      _onLoadCallbacks.push(callback);
-    }
-    return this;
-  };
-
-
-  var _flowSteps = {
+  var _flowActions = {
     'run': function (input, info) {
       var codeFn = new Function(input);
       codeFn.call(this);
@@ -60,12 +65,12 @@
       return [input, info];
     }
   };
-  function _runFlow (code, steps, name) {
+  function _applyFlow (code, steps, name) {
     var stage = code;
     var info = {name: name, code: code};
     for (var i = 0; i < steps.length; i++) {
-      if (_flowSteps.hasOwnProperty(steps[i])) {
-        var resp = _flowSteps[steps[i]].call(window, stage, info);
+      if (_flowActions.hasOwnProperty(steps[i])) {
+        var resp = _flowActions[steps[i]].call(window, stage, info);
         stage = resp[0];
         info = resp[1];
       } else if (steps[i]) {
@@ -76,12 +81,41 @@
   }
 
   function _register (name, fn) {
-    _flowSteps[name] = fn;
+    _flowActions[name] = fn;
   }
 
-  window.sat = _runFlow;
+  function onLoadWrapper (eventType, callback, useCapture) {
+    if (eventType === 'load') {
+      _onLoadCallbacks.push(callback);
+    }
+    return this;
+  };
+
+  if (!window.sat) {
+    window.addEventListener("sat:override", function (evt) {
+      evt.passCallbacks(_onLoadCallbacks, _flowActions);
+      _applyFlow = evt.applyFlow;
+    }, false);
+  } else {
+    var evt = document.createEvent("Events");
+    evt.initEvent('sat:override', true, true);
+    evt.applyFlow = _applyFlow;
+    evt.passCallbacks = function (_loadCBs, _flow) {
+      _onLoadCallbacks = _loadCBs;
+      var cbIndex = _onLoadCallbacks.indexOf(blocksAnyCall);
+      if (cbIndex !== -1) {
+        _onLoadCallbacks.splice(cbIndex, 1);
+      }
+      _flowActions = _flow;
+      window.addEventListener = onLoadWrapper;
+    };
+    document.dispatchEvent(evt);
+  }
+
+  window.addEventListener = onLoadWrapper;
+  window.sat = _applyFlow;
   window.sat.register =  _register;
   window.sat.adapters = function () {
-    return Object.keys(_flowSteps);
+    return Object.keys(_flowActions);
   }
 })(window, document);
